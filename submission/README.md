b3b163f8ed95f8f421e7b835b64cd66d6df6602d
GaneshArwan <wiliamzwili135@gmail.com>
2023-10-29 22:02:28 +0700

    Initial commit

---
+++ submission/README.md
@@ -0,0 +1,52 @@
+# Bike Sharing 
+Link Streamlit Cloud : [Bike Sharing Dashboard]()
+
+## Project installation
+Below is a step-by-step installation of the following project using the following command line options for the project itself:
+
+1. Clone this repository
+   ```
+   git clone https://github.com/GaneshArwans/bike_sharing.git
+   ```
+
+2. Move to directory bike_sharing
+   ```
+   cd bike_sharing
+   ```
+
+3. Move to directory submission
+   ```
+   cd submission
+   ```
+
+4. Create python virtual environment
+   ```
+   python -m venv bike_sharing_venv
+   ```
+
+5. Active environment
+   ```
+   bike_sharing_venv\Scripts\activate
+   ```
+
+6. Install all the requirements inside "requirements.txt"
+   ```
+   pip install -r requirements.txt
+   ```
+
+7. Install virtual environment
+   ```
+   python -m ipykernel install --name=bike_sharing_venv
+   ```
+
+8. Deactive Environment
+   ```
+   bike_sharing_venv\Scripts\deactivate
+   ```
+
+9. Run streamlit app
+   ```
+   streamlit run dashboard\dashboard.py
+   ```
+
+
---
+++ submission/dashboard/dashboard.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+
+def sidebar(df):
+    df["dteday"] = pd.to_datetime(df["dteday"])
+    min_date = df["dteday"].min()
+    max_date = df["dteday"].max()
+
+    with st.sidebar:
+        st.image("https://freepngimg.com/thumb/bicycle/6-2-bicycle-png-7.png")
+
+        def on_change():
+            st.session_state.date = date
+
+        date = st.date_input(
+            label="Rentang Waktu",
+            min_value=min_date,
+            max_value=max_date,
+            value=[min_date, max_date],
+            on_change=on_change
+        )
+    return date
+
+def season(df):
+    st.subheader("Rata-rata Jumlah Peminjaman Per Musim")
+    season_avg = df.groupby('season')['cnt'].mean().sort_index()
+    season_labels = ['Fall', 'Spring', 'Summer', 'Winter']
+    fig = plt.figure(figsize=(20, 10))
+    plt.bar(season_labels, season_avg)
+    plt.xlabel("Season", fontsize=18)
+    plt.ylabel("Rata-rata Jumlah Peminjaman", fontsize=18)
+    plt.title("Rata-rata Jumlah Peminjaman Per Musim", fontsize=20)
+    st.pyplot(fig)
+
+def workingday(df):
+    st.subheader("Rata-rata Jumlah Peminjaman Sepeda Per Jam")
+    hourly_rentals = df.groupby(["hr", "workingday"])["cnt"].mean().reset_index()
+    fig = plt.figure(figsize=(10, 6))
+    sns.barplot(data=hourly_rentals, x="hr", y="cnt", hue="workingday")
+    plt.title("Rata-rata Jumlah Peminjaman Sepeda per Jam",fontsize=18)
+    plt.xlabel("Jam dalam Sehari",fontsize=18)
+    plt.ylabel("Rata-rata Jumlah Peminjaman Sepeda",fontsize=15)
+    plt.xticks(rotation=0)
+    plt.legend(title="Working day")
+    plt.grid(axis="y")
+    st.pyplot(fig)
+
+if __name__ == "__main__":
+    sns.set(style="dark")
+    st.header("Bike Sharing Streamlit Dashboard")
+
+    day_df = pd.read_csv("day_clean.csv")
+    hour_df = pd.read_csv('hour_clean.csv')
+
+    date = sidebar(day_df)
+    if(len(date) == 2):
+        main_df = day_df[(day_df["dteday"] >= str(date[0])) & (day_df["dteday"] <= str(date[1]))]
+    else:
+        main_df = day_df[(day_df["dteday"] >= str(st.session_state.date[0])) & (day_df["dteday"] <= str(st.session_state.date[1]))]
+
+    season(main_df)
+    workingday(hour_df)
+
+    year_c = datetime.date.today().year
+    copyright = "Copyright © " + str(year_c) + " William "
+    st.caption(copyright)
+
